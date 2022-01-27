e12deb5508610d863ce75ed3c655b6d0f5f7f02e
mihaim07 <florynmogo@gmail.com>
2022-01-27 23:48:20 +0200

    New commit

--- script.js
+++ script.js
@@ -10,53 +10,64 @@ const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+let favorites = {};
 
-function updateDOM() {
-  resultsArray.forEach((result) => {
-    // Card Container
-    const card = document.createElement('div');
-    card.classList.add('card');
-    // Link
-    const link = document.createElement('a');
-    link.href = result.hdurl;
-    link.title = 'View Full Image';
-    link.target = '_blank';
-    // Image
-    const image = document.createElement('img');
-    image.src = result.url;
-    image.alt = 'NASA Picture of the Day';
-    image.loading ='lazy'; // !!! Incarcare progresiva a imaginilor
-    image.classList.add('card-img-top');
-    // Card Body
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-    // Card Title
-    const cardTitle = document.createElement('h5');
-    cardTitle.classList.add('card-title');
-    cardTitle.textContent = result.title;
-    // Save Text
-    const saveText = document.createElement('p');
-    saveText.textContent = 'Add to Favorites';
-    // Card Text
-    const cardText = document.createElement('p');
-    cardText.textContent = result.explanation;
-    // Footer Container
-    const footer = document.createElement('small');
-    footer.classList.add('text-muted');
-    // Date
-    const date = document.createElement('strong');
-    date.textContent = result.date;
-    // Copyright
-    const copyrightResult = result.copyright === undefined ? '' : result.copyright;
-    const copyright = document.createElement('span');
-    copyright.textContent = ` ${copyrightResult}`;
-    // Append
-    footer.append(date, copyright);
-    cardBody.append(cardTitle, saveText, cardText, footer);
-    link.appendChild(image);
-    card.append(link, cardBody);
-    imagesContainer.appendChild(card);
-  });
+function createDOMNodes(page) {
+    const currentArray = page === 'results' ? resultsArray : Object.values(favorites);
+    currentArray.forEach((result) => {
+        // Card Container
+        const card = document.createElement('div');
+        card.classList.add('card');
+        // Link
+        const link = document.createElement('a');
+        link.href = result.hdurl;
+        link.title = 'View Full Image';
+        link.target = '_blank';
+        // Image
+        const image = document.createElement('img');
+        image.src = result.url;
+        image.alt = 'NASA Picture of the Day';
+        image.loading ='lazy'; // !!! Incarcare progresiva a imaginilor
+        image.classList.add('card-img-top');
+        // Card Body
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        // Card Title
+        const cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = result.title;
+        // Save Text
+        const saveText = document.createElement('p');
+        saveText.textContent = 'Add to Favorites';
+        saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
+        // Card Text
+        const cardText = document.createElement('p');
+        cardText.textContent = result.explanation;
+        // Footer Container
+        const footer = document.createElement('small');
+        footer.classList.add('text-muted');
+        // Date
+        const date = document.createElement('strong');
+        date.textContent = result.date;
+        // Copyright
+        const copyrightResult = result.copyright === undefined ? '' : result.copyright;
+        const copyright = document.createElement('span');
+        copyright.textContent = ` ${copyrightResult}`;
+        // Append
+        footer.append(date, copyright);
+        cardBody.append(cardTitle, saveText, cardText, footer);
+        link.appendChild(image);
+        card.append(link, cardBody);
+        imagesContainer.appendChild(card);
+      });
+}
+
+function updateDOM(page) {
+    // Get Favorites from localStorage
+    if (localStorage.getItem('nasaFavorites')) {
+        favorites = JSON.parse(localStorage.getItem('nasaFavorites'));
+    }
+    createDOMNodes(page);
 }
 
 // Get 10 Images from NASA API
@@ -65,10 +76,27 @@ async function getNasaPictures() {
         const response = await fetch(apiUrl);
         resultsArray = await response.json();
         console.log(resultsArray);
-        updateDOM();
+        updateDOM('results');
     } catch (error) {
 
     }
+}
+
+// Add result to Favorites
+function saveFavorite(itemUrl) {
+    // Loop through results Array to select Favorite
+    resultsArray.forEach((item) => {
+        if (item.url.incudes(itemUrl) && !favorites[itemUrl]) {
+            favorites[itemUrl] = item;
+            // Show Save Confirmation for 2 Seconds
+            saveConfirmed.hidden = false;
+            setTimeout(() => {
+                saveConfirmed.hidden = true;
+            }, 2000);
+            // Set Favorites in localStorage
+            localStorage.setItem('nasaFavorites', JSON.stringify(favorites));
+        }
+    });
 }
 
 // On Load
